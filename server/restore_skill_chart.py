3424d67f568b06e1777b9408238203f29ccc050e
MiricleDev <rohan13k@outlook.com>
2026-01-14 10:38:52 -0500

    fr

--- server/restore_skill_chart.py
+++ server/restore_skill_chart.py
@@ -1,3 +1,8 @@
+import sys
+import os
+# Add parent directory to path so we can import server
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from server import app, db
 from server.db.models import SkillDevelopmentChart, Group
 from datetime import datetime
